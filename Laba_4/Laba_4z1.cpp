29f0c440e3636fbff4d0e6550450dafe09e12195
Andrei <trukhanovich_02@mail.ru>
2020-10-08 22:25:49 +0300

    Laba_4

---
+++ Laba_4/Laba_4z1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int strToInt(string s) {
+	int ans = 0;
+	for (auto ch : s) {
+		ans *= 10;
+		int c = (unsigned char)ch - (unsigned char)'0';
+		ans += c;
+		if (c < 0 || c > 9)
+			return 0;
+	}
+	return ans;
+}
+
+int main() {
+
+	ifstream fin{ "z1.txt" };
+
+	string s;
+	while (fin >> s) {
+		cout << strToInt(s) << "\n";
+	}
+}
---
+++ Laba_4/Laba_4z2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ifstream fin{ "z2.txt" };
+
+	char c;
+	map <char, int> m;
+
+	vector <char> ans; int r = 0;
+	vector <char> s;
+	while (fin >> noskipws >> c) {
+		if (c == ' ') {
+			int mx = 0;
+			for (auto c : m)
+				mx = max(c.second, mx);
+
+			if (mx > r) {
+				r = mx;
+				ans = s;
+			}
+
+			m.clear();
+			s.clear();
+			continue;
+		}
+		m[c]++;
+		s.push_back(c);
+	}
+
+	int mx = 0;
+	for (auto c : m)
+		mx = max(c.second, mx);
+
+	if (mx > r) {
+		r = mx;
+		ans = s;
+	}
+
+	for (auto c : ans)
+		cout << c;
+
+}
+
