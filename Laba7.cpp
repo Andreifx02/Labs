29f7a12a1c052306723007f93fe6829eb0fc1f36
Andrei <trukhanovich_02@mail.ru>
2020-11-13 20:02:47 +0300

    Laba7

---
+++ Laba7.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+#include "students.h"
+
+using namespace std;
+
+int main() {
+	Student* a = new Student((char*)"David", 1, 3, 1749);
+	Student* b = new Student1((char*)"sadsa", 1, 1, 1, vector<int>{1, 2, 3, 4}); 
+	Student* c = new Student2((char*)"sadsa", 1, 1, 1, vector<int>{1, 2, 3, 6}, vector<int>{1, 2, 3, 45}); 
+
+	vector<Student*> students = { a, b, c };
+
+	cout << CalcMeanForArray(students) << endl;
+	cout << *a << endl;
+	return 0;
+}
---
+++ students.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static int g_id = 1000000;
+
+class Student {
+public:
+	Student(const Student& a);
+
+	Student(char* Name, int Course, int Group, int NumZ);
+
+	int getId() { return id; }
+	int getNumZ() { return numz; }
+	int getCourse() { return course; }
+	int getGroup() { return group; }
+	char* getName() { return name; }
+
+	void setGroup(int new_group) { group = new_group; }
+	void setName(char* new_name) { name = new_name; }
+	void setCourse(int new_course) { course = new_course; }
+
+	friend ostream& operator<< (ostream& os, const Student& a);
+
+	virtual double CalcMean() {
+		return -1;
+	}
+
+protected:
+	const int id = 0;
+	const int numz = 0;
+	char* name;
+	int course;
+	int group;
+
+	Student() {}
+};
+
+class Student1 : public Student {
+public:
+	Student1(const Student1& a);
+
+	Student1(char* Name, int Course, int Group, int NumZ, vector<int> new_grade1);
+
+	double CalcMean() override {
+		int sum = 0;
+		int cnt = 0;
+		for (int i = 0; i < 4; i++) {
+			sum += grade1[i];
+			cnt++;
+		}
+		return sum / cnt;
+	}
+
+	int getGrade1(int number) { return grade1[number - 1]; }
+	void setGrade1(int number, int new_grade) { grade1[number - 1] = new_grade; }
+
+	friend ostream& operator<< (ostream& os, const Student1& a);
+
+protected:
+	vector<int> grade1;
+	Student1() {}
+};
+
+class Student2 : public Student1 {
+public:
+	Student2(const Student2& a);
+
+	Student2(char* Name, int Course, int Group, int NumZ, vector<int> new_grade1, vector<int> new_grade2);
+
+	double CalcMean() override {
+		int sum = 0;
+		int cnt = 0;
+		for (int i = 0; i < 5; i++) {
+			sum += grade2[i];
+			cnt++;
+		}
+		return sum / cnt;
+	}
+
+	int getGrade2(int number) { return grade2[number - 1]; }
+	void setGrade2(int number, int new_grade) { grade2[number - 1] = new_grade; }
+private:
+	vector<int> grade2;
+	Student2() {}
+};
+
+double CalcMeanInGroup(vector<Student2> v, int group);
+
+double CalcMeanForArray(vector<Student*> students);
